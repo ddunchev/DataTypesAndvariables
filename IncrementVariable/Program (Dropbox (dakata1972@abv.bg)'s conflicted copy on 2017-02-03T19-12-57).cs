082a46a4acc36ab07a0b6f52dd93d97bb676a7c8
ddunchev <danchodunchev@gmail.com>
2017-02-03 21:39:15 +0200

    Write a Increment variable program

---
+++ IncrementVariable/Program (Dropbox (dakata1972@abv.bg)'s conflicted copy on 2017-02-03T19-12-57).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = byte.Parse(Console.ReadLine());
+
+            for (byte i = 0; i <= number; i++)
+            {
+                try
+                {
+                    Console.WriteLine(i);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ IncrementVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IncrementVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int incrementTimes = int.Parse(Console.ReadLine());
+            byte num = 0;
+            int count = 0;
+
+            for (int i = 0; i < incrementTimes; i++)
+            {
+                num++;
+                if (num == 0) count++;
+            }
+
+            Console.WriteLine(num);
+
+            if (count > 0) Console.WriteLine("Overflowed {0} times", count);
+        }
+    }
+}
