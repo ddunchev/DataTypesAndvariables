13ba5b8c8c52321961cfd3ccb7abe73213d92420
ddunchev <danchodunchev@gmail.com>
2017-02-03 20:50:30 +0200

    This is a first commit

---
+++ DistanceOfTheStars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceOfTheStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal proximaCentauri = 4.22m;
+            decimal milkyWay = 26000m;
+            decimal diameterOfMilkyWay = 100000m;
+            decimal toTheEdge = 46500000000m;
+
+            Console.WriteLine($"{proximaCentauri * 9450000000000:0.##e+000}");
+            Console.WriteLine($"{milkyWay * 9450000000000:0.##e+000}");
+            Console.WriteLine($"{diameterOfMilkyWay * 9450000000000:0.##e+000}");
+            Console.WriteLine($"{toTheEdge * 9450000000000:0.##e+000}");
+
+
+
+
+        }
+    }
+}
