e9e15cdcedb5f69ea63e9cd94ad3f0bcc4e9b187
ddunchev <danchodunchev@gmail.com>
2017-02-04 10:07:37 +0200

    Commint Incremen

---
+++ TerabitesToBites/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+namespace TerabitesToBites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:0.#}", (decimal)(input * 1024 * 1024 * 1024 * 1024 * 8));
+        }
+    }
+}
